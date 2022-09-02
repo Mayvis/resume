2eb7838a002f3c32fbedda612c9e1338b9b4cb98
Mayvis <kevin73911@gmail.com>
2022-09-02 15:41:18 +0800

    chore: add error boundary

--- src/main.tsx
+++ src/main.tsx
@@ -2,26 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { Global, css } from '@emotion/react'
+import ErrorBoundary from './components/ErrorBoundary'
 import reset from './utils/reset'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Global
-      styles={css`
-        // import reset css globally
-        ${reset}
+    <ErrorBoundary>
+      <Global
+        styles={css`
+          // import reset css globally
+          ${reset}
 
-        * {
-          box-sizing: border-box;
-        }
+          * {
+            box-sizing: border-box;
+          }
 
-        body {
-          font-family: 'Poppins', sans-serif;
-          -webkit-font-smoothing: antialiased;
-          -moz-osx-font-smoothing: grayscale;
-        }
-      `}
-    />
-    <App />
+          body {
+            font-family: 'Poppins', sans-serif;
+            -webkit-font-smoothing: antialiased;
+            -moz-osx-font-smoothing: grayscale;
+          }
+        `}
+      />
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>
 )
